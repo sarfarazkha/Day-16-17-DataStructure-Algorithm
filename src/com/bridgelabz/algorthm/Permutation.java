18bb13340c491872f701383a81e4dc2de7e443e3
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2022-06-14 23:04:11 +0530

    String permutation

---
+++ src/com/bridgelabz/algorthm/Permutation.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.algorthm;
+
+public class Permutation {
+    static void permutate(String str, String ans) {
+        if (str.length() == 0) {
+            System.out.print(ans + " ");
+            return;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            String ros = str.substring(0, i) + str.substring(i + 1);
+            permutate(ros, ans + ch);
+        }
+    }
+
+    public static void main(String[] args) {
+        String s = "abc";
+        permutate(s, "");
+    }
+}
