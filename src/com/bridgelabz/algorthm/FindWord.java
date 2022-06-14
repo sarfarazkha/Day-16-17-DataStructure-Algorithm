c58b01c4e06646842bac4b7ebe57f30f3d4e2a2d
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2022-06-14 23:22:43 +0530

    FindWord

---
+++ src/com/bridgelabz/algorthm/FindWord.java
@@ -0,0 +1,40 @@
+package com.bridgelabz.algorthm;
+
+
+import java.util.Arrays;
+
+public class FindWord {
+
+    static int find(String search, String[] findWord, int l, int length) {
+        if (length >= l) {
+            int mid = l + (length - l) / 2;
+            if (findWord[mid] == search) {
+                return mid;
+            } else if (findWord[mid].compareTo(search) > 0) {
+                return find(search, findWord, l, mid - 1);
+            } else {
+                return find(search, findWord, mid + 1, length);
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String findWord[] = {"Ramu", "Rajneesh", "Mohan", "Rohan", "sarfaraz"};
+        String search = "Ramu";
+
+        Arrays.sort(findWord);
+
+        for (String x : findWord) {
+            System.out.print(x + " ");
+        }
+
+        int i = find(search, findWord, 0, findWord.length - 1);
+        if (i == -1) {
+            System.out.println("Word is not in the Array");
+        } else {
+            System.out.println("\nWord found at sorted Array of index : " + (i + 1));
+        }
+
+    }
+}
